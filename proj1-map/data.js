546d9be32698afaa349965d1b97c69b2bab1ac02
theresamm <mtheresamayo@gmail.com>
2022-06-23 14:58:24 +0000

    foursquare map layout

---
+++ proj1-map/data.js
@@ -0,0 +1,19 @@
+const BASE_API_URL = "https://api.foursquare.com/v3";
+const API_KEY = "fsq3oPWCqp/KrOvP2k17DQSJgN9+gFHhlSkl1CcobbFQt48=";
+const headers = {
+    "Accept":'application/json',
+    "Authorization":API_KEY
+}
+async function search(lat, lng, query){
+    let ll = lat+","+lng;
+    let response = await axios.get(BASE_API_URL + "/places/search",{
+        "headers": headers,
+        "params":{
+            'll':ll,
+            'query':query
+        
+        }
+    })
+    return response.data;
+}
+
--- proj1-map/script.js
+++ proj1-map/script.js
@@ -1,12 +1,8 @@
-let map = L.map("map");
-let singapore = [1.3521, 103.8198];
-map.setView(singapore, 12);
-let tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
-});
-tileLayer.addTo(map);
+function main() {
+   function init(){
+        let map = initMap();
+   }
+   
+    init();
+}
+main();
