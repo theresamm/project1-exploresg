aa6ec3432377f03d3f5aa7fa61b0d537cda44823
theresamm <mtheresamayo@gmail.com>
2022-06-24 12:35:48 +0000

    map markers

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function main() {
     function init(){
          let map = initMap();
+
+         let searchResultLayer = L.layerGroup();
+         searchResultLayer.addTo(map);
+
+         window.addEventListener("DOMContentLoaded", function (){
+            document.querySelector("#btnsearch").addEventListener('click', async function(){
+                let query = document.querySelector("#textSearch").value;
+                let center = map.getBounds().getCenter();
+                let data = await search(center.lat, center.lng, query);
+                for (let result of data.results){
+                    let latlng = [result.geocodes.main.latitude, result.geocodes.main.longitude];
+                    let resultMarker = L.marker(latlng);
+                    resultMarker.bindPopup(`
+                        <h2>${result.name}</h2>
+                    `)
+                    resultMarker.addTo(searchResultLayer);
+                }
+            })
+         })
     }
     
      init();
